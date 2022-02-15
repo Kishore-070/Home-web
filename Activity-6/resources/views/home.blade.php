f545682fbfef898925b9e20f5b1d84d5c9f97d71
Kishore <98384128+Kishore-070@users.noreply.github.com>
2022-02-16 00:53:27 +0530

    Add files via upload

---
+++ Activity-6/resources/views/home.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <title>Portfolio</title>
+
+    <style>
+        body{
+            background-image: url(/images/portfolioimg.gif);
+            background-size: cover;
+        }
+        .abc{
+            margin-top:170px;
+            font-family: "Monaco";
+        }
+        .ab{
+            margin-top:120px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+    <div class="row">
+        <div class="col pt-2">
+            <h1 class="text-info"><i class="fa fa-spinner">PORTFOLIO</i></h1>
+        </div>
+     <div class="col-1">
+       <a class="nav-link" href="/about"><button type="button" class="btn btn-dark">About</button></a>
+    </div>
+     <div class="col-2">
+       <a class="nav-link" href="/achievements"><button type="button" class="btn btn-dark">Achievements</button></a>
+    </div>
+    </div>
+
+    <div class="row" >
+        <div class="col abc">
+            <h1 class="display-1 text-primary"><i>Hii I'm Kishore</i></h1>
+            <h1 class="text-info">A Budding Computer Science Engineer</h1>
+        </div>
+        <div class="col-6 ab">
+            <img src="/images/hacker.jpg" alt="PICTURE" height="500" width="500">
+        </div>
+    </div>
+     </div>
+     
+</body>
+</html>
