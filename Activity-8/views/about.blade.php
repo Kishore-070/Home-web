bdd7913ed57250892ce7d0351572a5e1ba42ba0f
Kishore <98384128+Kishore-070@users.noreply.github.com>
2022-02-24 11:26:40 +0530

    Add files via upload

---
+++ Activity-8/views/about.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>About</title>
+
+    <style>
+        body{
+            background-image:url(/images/backgroundabout.gif);
+            background-size:cover;
+        }
+        .about1{
+            background-color:rgb(72, 141, 221);
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1 class="pt-4 d-flex justify-content-center display-1"><i class="fa fa-refresh text-primary"> ABOUT <i class="fa fa-refresh"></i></i></i></h1>
+
+        <div class="about1 rounded p-5 mt-5 border border-5 border-primary">
+            <h1 class="">An inquistive,energitic Budding computer science and engineer skilled in leadership,with some good foundation n programmic logic seeking to leverage good development skills and creativity as a programmer</h1>
+        </div>
+
+        <div class="d-flex justify-content-between mt-5">
+            <div class="bg-secondary me-5 ps-4 rounded p-2">
+                <h1 class="d-flex justify-content-center bg-dark pe-3 me-4 text-secondary">Skills</h1>
+                <h3>
+                    <ul>
+                        <li>Good Communication Skills</li>
+                        <li>Good Problem Solving Skills</li>
+                        <li>Good Leadership Skill</li>
+                        <li>Good in programming laguages</li>                 
+                    </ul>
+                </h3>
+            </div>
+            <div class="bg-secondary ms-5 ps-4 rounded p-2">
+                <h1 class="d-flex justify-content-center bg-dark pe-3 me-4 text-secondary">Hobby</h1>
+                <h3>
+                    <ul>
+                        <li>Likes to play all kinds of games</li>
+                        <li>Watching Cricket</li>
+                        <li>Watchng Football</li>
+                        <li>Travel</li>
+                    </ul>
+                </h3>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
